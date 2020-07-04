ca5b17136f2c76e8999b1fec0337e7427d011e67
Jacob Amaral <jake0303@hotmail.ca>
2020-07-04 11:13:49 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# TDAmeritradeDiscordBot
+
+
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+﻿
+// Dependencies
+var http = require('http');
+var request = require('request');
+var express = require('express');
+
+var app = express();
+const redirect_uri = '<YOUR-CALLBACK-URL>';
+
+/* 
+Callback endpoint the TDA app uses.
+To understand more about how the API authenticates, see this link.
+https://developer.tdameritrade.com/content/simple-auth-local-apps
+*/
+app.get('/auth', (req, res) => {
+    var authRequest = {
+        url: 'https://api.tdameritrade.com/v1/oauth2/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            'grant_type': 'authorization_code',
+            'access_type': 'offline',
+            'code': req.query.code, // get the code from url
+            'client_id': process.env.CLIENT_ID + "@AMER.OAUTHAP", // this client id comes from config vars
+            'redirect_uri': redirect_uri
+        }
+    };
+
+    // make the post request
+    request(authRequest, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // parse the tokens
+            var authReply = JSON.parse(body);
+            // to check it's correct, display it
+            res.send(authReply);
+        }
+    });
+});
+
+// start server
+var httpServer = http.createServer(app);
+var port = process.env.PORT || 8080;
+httpServer.listen(port, () => {
+    console.log(`Listening at ${port}`);
+});
+view rawtd - auth.js hosted with ❤ by GitHub
