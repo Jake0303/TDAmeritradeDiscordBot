33a95f172ef7663bf660b554d8cc7ce40add6110
Jacob Amaral <jake0303@hotmail.ca>
2020-07-31 12:16:20 -0400

    Encoded License Key

--- public/javascripts/licensekey.js
+++ public/javascripts/licensekey.js
@@ -5,7 +5,7 @@
             type: 'POST',
             url: '/generateLicenseKey',
             success: function (data) {
-                $('#createLicenseKeyForm').after('<hr><h3>A new license key has been generated : <big>' + data.licenseKey + "</big></h3><h2 style='word-break:break-all;'>Use this link : <a href='https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https%3A%2F%2Fdiscordbottrades.herokuapp.com%2Fauth&licenseKey=" + data.licenseKey + "&client_id=P3FYOWCFDPAYMPS1UKGR2O0AVOCDRLGA%40AMER.OAUTHAP'>https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https%3A%2F%2Fdiscordbottrades.herokuapp.com%2Fauth&licenseKey=" + data.licenseKey + "&client_id=P3FYOWCFDPAYMPS1UKGR2O0AVOCDRLGA%40AMER.OAUTHAP</a></h2>");
+                $('#createLicenseKeyForm').after('<hr><h3>A new license key has been generated : <big>' + data.licenseKey + "</big></h3><h2 style='word-break:break-all;'>Use this link : <a href='https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https%3A%2F%2Fdiscordbottrades.herokuapp.com%2Fauth%2FlicenseKey=" + data.licenseKey + "&client_id=P3FYOWCFDPAYMPS1UKGR2O0AVOCDRLGA%40AMER.OAUTHAP'>https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https%3A%2F%2Fdiscordbottrades.herokuapp.com%2Fauth%2FlicenseKey=" + data.licenseKey + "&client_id=P3FYOWCFDPAYMPS1UKGR2O0AVOCDRLGA%40AMER.OAUTHAP</a></h2>");
             }
         });
     });
