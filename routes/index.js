29538330b703479e74535c2edb1ddf45b471c0db
Jake0303 <jake0303@hotmail.ca>
2020-09-24 14:07:54 -0400

    Added more debugging

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,9 @@ client.on('message', function (message) {
                         strike = parseInt(split[7]);
                         callput = split[8].charAt(0).toUpperCase();
                         price = split[10];
-                        symbol = symbol + "_" + moment(month + day + year).format("MMDDYY") + callput + strike;
+                        symbol = symbol + "_" + moment(month + day + year).format("MMMDDYY") + callput + strike;
+                        print(month + day + year);
+
                         console.log(symbol);
                     }
 
